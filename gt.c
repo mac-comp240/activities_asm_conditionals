b7ed3fd7dad2a034841388fd8040c45b58887335
Susan Eileen Fox <fox@macalester.edu>
2023-08-03 10:40:00 -0500

    Add files via upload

---
+++ gt.c
@@ -0,0 +1,19 @@
+
+// returns 1 if x > y
+int gt (long x, long y)
+{
+  return x > y;
+}
+// why is it just fine for this to return an int?
+
+// returns 1 if x is equal to zero
+int zero(long x) {
+    return x == 0L;   // what does the L do?
+}
+
+// TODo: 
+// Now experiment below here with the rest of the relational operators
+// in new functions:
+// >=  <  <=  ==  !=
+// 
+// also try different data types
